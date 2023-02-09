eb91cc3cf255344d101d0ed3b534239bb0c1d010
ayoubbelhadjyahia <belhadjyahiaayoub2@gmail.com>
2023-02-09 22:03:24 +0100

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailproductComponent } from './detailproduct/detailproduct.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'products',pathMatch:'full'},
+  {path:'products',component:ProductComponent},{path:'emplois',component:OffresEmploiComponentComponent},
+  {path:'product/:id',component:DetailproductComponent},
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailproductComponent } from './detailproduct/detailproduct.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    OffresEmploiComponentComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    DetailproductComponent
   ],
   imports: [
     BrowserModule,
